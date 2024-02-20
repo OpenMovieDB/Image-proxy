ab7bd8fc86f4f744bf444226d741206d68796798
mdwitr0 <mdwit0r@gmail.com>
2024-02-20 14:24:51 +0100

    chore: add avif options

--- converter/avif.go
+++ converter/avif.go
@@ -34,10 +34,7 @@ func (w *Avif) Convert(reader io.Reader, quality float32, f func(img image.Image
 	// переводим качество из диапазона 0-100 в диапазон 0-63
 	qualityAiff := 63 - int(quality/100*63)
 
-	if err := avif.Encode(&buf, img, &avif.Options{
-		Speed:   8,
-		Quality: qualityAiff,
-	}); err != nil {
+	if err := avif.Encode(&buf, img, &avif.Options{Threads: 0, Speed: 8, Quality: qualityAiff}); err != nil {
 		slog.Error(err.Error())
 		return nil, err
 	}
